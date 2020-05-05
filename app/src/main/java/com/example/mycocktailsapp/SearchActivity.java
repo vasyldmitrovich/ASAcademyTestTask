b941922e8d30e84f5435944883948715f69ce7b8
VasylDmitrovich <Rapidvasyaalister@gmail.com>
2020-05-05 17:06:54 +0300

    Clean some unnecessary code, and delete unnecessary file

--- app/src/main/java/com/example/mycocktailsapp/SearchActivity.java
+++ app/src/main/java/com/example/mycocktailsapp/SearchActivity.java
@@ -37,21 +37,15 @@ public class SearchActivity extends AppCompatActivity {
         recyclerAdapter = new RecyclerAdapter(getApplicationContext(), drinkList);
         recyclerView.setAdapter(recyclerAdapter);
 
-
+        //TODO instead string "margarita" in next line. I am doing search bar and input data from them
         Call<DrinkList> call = NetworkService.getInstance().getJSONApi().getDrinkList("margarita");
         call.enqueue(new Callback<DrinkList>() {
             @Override
             public void onResponse(Call<DrinkList> call, Response<DrinkList> response) {
                 if (response.isSuccessful()) {
+                    DrinkList drinkListFromResponse = response.body();
 
-                    DrinkList drinkListTemp = response.body();
-                    /*Temp*/
-                    List<Drink> drinks = drinkListTemp.getDrinks();
-                    for (Drink d: drinks
-                    ) {
-                        System.out.println("This is DRINK: "+d.getStrDrink()+"\n");
-                    }
-                    drinkList = drinkListTemp.getDrinks();
+                    drinkList = drinkListFromResponse.getDrinks();
                     recyclerAdapter.setDrinkList(drinkList);
                 }
                 else {
@@ -63,48 +57,5 @@ public class SearchActivity extends AppCompatActivity {
                 System.out.println("Sorry gay error. "+t.toString());
             }
         });
-
-/*
-
-        //This code work, he was before i start doing RecyclerView
-        final TextView textView = findViewById(R.id.textViewSearch);
-        TextView textView1 = findViewById(R.id.textViewName);
-        ImageView imageView = (ImageView)findViewById(R.id.imageID);
-
-        NetworkService.getInstance()
-                .getJSONApi()
-                .getDrinkList("margarita")
-                .enqueue(new Callback<DrinkList>() {
-                    @Override
-                    public void onResponse(Call<DrinkList> call, Response<DrinkList> response) {
-                        if (response.isSuccessful()) {
-                            System.out.println("Response is successful: "+ response.body());
-                            DrinkList drinkList = response.body();
-                            List<Drink> drinks = drinkList.getDrinks();
-                            ArrayList<String> arrayList = new ArrayList<>();
-                            for (Drink drink: drinks
-                                 ) {
-                                System.out.println("This is message: "+drink.toString()+"\n");
-                                textView.append(" "+drink.getStrDrink()+"\n");
-                                textView1.append(" "+drink.getStrDrink()+"\n");
-                                arrayList.add(drink.getStrDrink());
-                            }
-                            Picasso.get().load(
-                                    "https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg")
-                                    .into(imageView);
-                        }
-                        else {
-                            System.out.println("Response is bad: "+response.code());
-                        }
-                    }
-                    @Override
-                    public void onFailure(Call<DrinkList> call, Throwable t) {
-                        textView.append(" Error sorry gay");
-                        t.printStackTrace();
-                    }
-                });
-*/
-
     }
-
 }
